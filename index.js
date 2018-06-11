afc92d7b4902ddda16d0d02d2cf3b606350fc81d
Joshua Suskin <jsuskin85@gmail.com>
2018-06-11 01:32:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var value;
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {[key]: value});
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  
+}
